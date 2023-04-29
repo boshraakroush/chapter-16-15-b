d6647bc3f0693ab4d72009c090f3beffa29aafae
Boshra Akroush <boshra.akroush@sinclair.edu>
2023-04-28 22:19:20 -0400

    first commit template and main functions done

--- chapter 16 b/code b Source.cpp
+++ chapter 16 b/code b Source.cpp
@@ -6,3 +6,29 @@
 #include <string>
 using namespace std;
 
+template <typename T>
+T half(T num)
+{
+	return num / 2;
+}
+
+int ihalf(int i)
+{	
+	static_cast<float>(i);
+	float x = i/ 2.0; 
+	float rounded= round(x);
+	
+	return rounded;
+}
+
+int main()
+{
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3; 
+
+	cout << half(a) << endl;
+	cout << half(b) << endl; 
+	cout << ihalf(c) << endl; 
+
+}
